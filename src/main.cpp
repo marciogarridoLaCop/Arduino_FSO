cc96d756c1cdbb3b31904ce767ca2d7d968a773d
Marcio Garrido <marciogarrido@MacBook-Pro-de-Marcio-Garrido.local>
2021-07-18 20:16:00 -0300

    Ajutado funcao retardo

--- src/main.cpp
+++ src/main.cpp
@@ -16,46 +16,17 @@ unsigned long tempo = 0;
 
 void setup()
 {
-
   Serial.begin(9600);
-  //Wire.begin();
-  //RTC.begin();
-  //if (! RTC.isrunning()) {
-  //  Serial.println("RTC não esta funcionando!");
-  //  RTC.adjust(DateTime(__DATE__, __TIME__));
 }
-// Serial.println("Iniciando SD...");
-// if (!SD.begin(chipSelect)) {
-//   Serial.println("Falha no SD, ou nao esta inserido");
-//   return;
-//}
-// Serial.println("SD iniciado");
-// now = RTC.now();
-// dataFile = SD.open("log.txt", FILE_WRITE);
-// dataFile.print("Iníncio da gravação: ");
-// dataFile.print(now.day(), DEC);
-// dataFile.print('/');
-// dataFile.print(now.month(), DEC);
-// dataFile.print('/');
-// dataFile.print(now.year(), DEC);
-//dataFile.println(" ");
-// dataFile.print(" - ");
-// dataFile.print(now.hour(), DEC);
-// dataFile.print(":");
-// dataFile.print(now.minute(), DEC);
-// dataFile.print(":");
-// dataFile.println(now.second(), DEC);
-// /dataFile.println(" ");
-// dataFile.close();
-//}
 
 void gerar_texto(int retardo)
 {
   tempo = millis();
   while (millis() < tempo + retardo)
   {
+  //Aguarda sem parar o processamento
   }
-  // now = RTC.now();
+  
   Serial.print(" Photo Central");
   Serial.print(" -> ");
   Serial.print(0);
@@ -81,8 +52,8 @@ void gerar_valores(int retardo)
   tempo = millis();
   while (millis() < tempo + retardo)
   {
+  //Aguarda sem parar o processamento
   }
-  // now = RTC.now();
   Serial.print(" ");
   Serial.print(0);
   Serial.print(lerphoto10(0));
