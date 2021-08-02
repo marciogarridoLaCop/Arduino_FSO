d283b76da8862293d9cdc079d94b6185bdd437b4
Marcio Garrido <marciogarrido@MacBook-Pro-de-Marcio-Garrido.local>
2021-08-02 14:25:44 -0300

    All code in inglish with axle dimession

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 /* DISSERTACAO DE MESTRADO EM ENGENHARIA ELETRICA E TELECOMUNICACOES - UFF 2018.2 
-   ALUNO:MARCIO ALEXANDRE DIAS GARRIDO
-   PROFESSOR DR. VINICIUS NUNES
+	 ALUNO:MARCIO ALEXANDRE DIAS GARRIDO
+	 PROFESSOR DR. VINICIUS NUNES
 */
 #include "RTClib.h"
 const int chipSelect = 10;
@@ -11,92 +11,108 @@ RTC_DS1307 RTC;
 #include <photodetector10.h>
 File dataFile;
 DateTime now;
-unsigned long tempo = 0;
-int incomingByte = 0;	// para dados seriais que estão entrando
-int passos,leitura= 0;
-String conteudo = "";
+unsigned long time = 0;
+String information = "";
+double x_position,steps=0;
+float axle =55;
+
 void setup()
 {
-  Serial.begin(9600);
-}
+	Serial.begin(9600); 
+	Serial.flush();
+	Serial.println ("Press enter to reading values");
+	x_position=(-(axle/2));
+} 
 
-void gerar_texto(int retardo)
+void generate_text(int delay)
 {
-  tempo = millis();
-  while (millis() < tempo + retardo)
-  {
-  //Aguarda sem parar o processamento
-  } 
-  Serial.print(" Photo Central");
-  Serial.print(" -> ");
-  Serial.print(0);
-  Serial.print(" Valor: ");
-  Serial.print(readphoto10(0));
-  for (int i = 1; i <= 3; i++)
-  {
-    Serial.print(" Photo");
-    Serial.print(" -> ");
-    Serial.print(i);
-    Serial.print(" Valor: ");
-    Serial.print(readphoto10(i));
-    if ((i) == 3)
-    {
-      Serial.println();
-    }
-  }
-  Serial.println();
+	time = millis();
+	while (millis() < time + delay)
+	{
+	//Wait without stop processesing 
+	} 
+	Serial.print(" Center Photo");
+	Serial.print(" -> ");
+	Serial.print(0);
+	Serial.print(" Value: ");
+	Serial.print(readphoto10(0));
+	for (int i = 1; i <= 3; i++)
+	{
+		Serial.print(" Photo");
+		Serial.print(" -> ");
+		Serial.print(i);
+		Serial.print(" Value: ");
+		Serial.print(readphoto10(i));
+		if ((i) == 3)
+		{
+			Serial.println();
+		}
+	}
+	Serial.println();
 }
 
-void gerar_valores(int retardo)
+void generete_values(int delay)
 {
-  tempo = millis();
-  while (millis() < tempo + retardo)
-  {
-  //Aguarda sem parar o processamento
-  }
-  //Serial.print(" ");
-  //Serial.print(0);
-  Serial.print(readphoto10(0),2);
-  //Serial.print(" ");
-  /*
-  for (int i = 1; i <= 3; i++)
-  {
-    Serial.print(readphoto10(i));
-    Serial.print(" ");
-    if ((i) == 3)
-    {
-      Serial.println();
-    }
-  }
-  */
-  Serial.println();
+	time = millis();
+	while (millis() < time + delay)
+	{
+	//Aguarda sem parar o processamento
+	}
+	 
+	
+	if (steps > abs((x_position*2))) {
+		Serial.print("Finished");
+	}
+	else
+	{
+	Serial.print(readphoto10(0),2);
+	Serial.print(" ");
+	Serial.print(x_position+steps);
+	steps=steps+1;
+	}
+	
+	/*
+	for (int i = 1; i <= 3; i++)
+	{
+		Serial.print(readphoto10(i));
+		Serial.print(" ");
+		if ((i) == 3)
+		{
+			Serial.println();
+		}
+	}
+	*/
+	Serial.println();
 }
 
-void leStringSerial(){
-  Serial.flush();
-  char caractere;
-  conteudo="";
-  // Enquanto receber algo pela serial
-  while(Serial.available() > 0) {
-    // Lê byte da serial
-    caractere = Serial.read();
-    // detecta o enter
-    if (caractere == '\n'){
-      // Concatena valores
-     gerar_valores(0);
-      conteudo.concat(caractere);
-    }
-    // Aguarda buffer serial ler próximo caractere
-    delay(10);
-  }  
+
+
+void read_serial_string(){
+	Serial.flush();
+	char character;
+	information="";
+	while(Serial.available() > 0) {
+		// read byte
+		character = Serial.read();
+		// check if return keyboard ware pressed
+		if (character == '\n'){
+			// Concat values
+		 generete_values(0);
+		 information.concat(character);
+		// wait buffer to read the next packet
+		delay(10);
+
+		}  
+	}
 }
+
 
 void loop()
 {
-  
-  if (Serial.available())
-    {
-    leStringSerial();
-    }
-  
+	if (Serial.available())
+	 
+		{
+	 read_serial_string();
+	 
+		}
 }
