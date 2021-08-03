738d9ac160946dfd4701d35ecd17876e999d6ede
Marcio Garrido <marciogarrido@MacBook-Pro-de-Marcio-Garrido.local>
2021-08-03 11:23:05 -0300

    All functions included in lib photodetector10

--- src/photodetector10.h
+++ src/photodetector10.h
@@ -11,7 +11,10 @@ this code should use F_CPU to determine the prescaled factor.
 
 int readvalue = 0; //Ler valor porta
 float volts = 0; //Valor convertido em volts (V)
-
+String information = "";
+double x_position, steps = 0;
+unsigned long time = 0;
+bool start;
 
 float readphoto10(int porta){
   readvalue = analogRead(porta);
@@ -19,4 +22,125 @@ float readphoto10(int porta){
   if (volts > 5)
     volts = 0;
   return volts;
+}
+void generete_values(int detector, int delay)
+{
+	if (start == false)
+	{
+		start = true;
+		switch (detector)
+		{
+		case 1:
+			Serial.print("  y1");
+			Serial.print(" ");
+			Serial.print("   x");
+			break;
+		case 2:
+			Serial.print("  y1");
+			Serial.print(" ");
+			Serial.print("  y2");
+			Serial.print(" ");
+			Serial.print("   x");
+			break;
+		case 3:
+			Serial.print("  y1");
+			Serial.print(" ");
+			Serial.print("  y2");
+			Serial.print(" ");
+			Serial.print("  y3");
+			Serial.print(" ");
+			Serial.print("   x");
+			break;
+		case 4:
+			Serial.print("  y1");
+			Serial.print(" ");
+			Serial.print("  y2");
+			Serial.print(" ");
+			Serial.print("  y3");
+			Serial.print(" ");
+			Serial.print("  y4");
+			Serial.print(" ");
+			Serial.print("   x");
+			break;
+		}
+		Serial.println("");
+	}
+
+	{
+		time = millis();
+		while (millis() < time + delay)
+		{
+			//Aguarda sem parar o processamento
+		}
+		if (detector == 1)
+		{
+			if (steps > abs((x_position * 2)))
+			{
+				steps = steps + 1;
+				Serial.print("Finished");
+				Serial.print(" ");
+				Serial.println();
+			}
+			else
+			{
+				Serial.print(readphoto10(detector - 1), 2);
+				Serial.print(" ");
+				Serial.print(x_position + steps - 1);
+				Serial.println();
+			}
+		}
+
+		if (detector > 1)
+		{
+			if (steps > abs((x_position * 2)))
+			{
+				steps = steps + 1;
+				Serial.print("Finished");
+				Serial.print(" ");
+				Serial.println();
+			}
+			else
+			{
+				for (int i = 1; i <= detector; i++)
+				{
+					Serial.print(readphoto10(i - 1), 2);
+					Serial.print(" ");
+					if ((i) == detector)
+					{
+					}
+				}
+				steps = steps + 1;
+			}
+		}
+		if (steps > abs((x_position * 2) - 1))
+		{
+		}
+		else
+		{
+			Serial.print(x_position + steps - 1);
+			Serial.println();
+		}
+	}
+}
+
+void read_serial_string(int n_off_detector)
+{
+	if (start == false)
+		Serial.flush();
+	char character;
+	information = "";
+	while (Serial.available() > 0)
+	{
+		// read byte
+		character = Serial.read();
+		// check if return keyboard ware pressed
+		if (character == '\n')
+		{
+			// Concat values
+			generete_values(n_off_detector, 0);
+			information.concat(character);
+			// wait buffer to read the next packet
+			delay(10);
+		}
+	}
 }
