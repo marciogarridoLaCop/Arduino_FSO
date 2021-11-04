91bdc3e367e7fae2ddd242b9de6584902689e26c
Marcio <marciogarrido@id.uff.br>
2021-11-04 13:10:47 -0300

    Nova Lib temperatura

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,17 @@
 #include <unophoto.h>
 #include <Statistic.h>
 #include <sensor_temp.h>
+#include <Thermistor3.h>
 Statistic myStats[6];
+Thermistor temp1(4);
+Thermistor temp2(5);
 
 bool simulated;
 int interacoes;
+double resistor = 10000;
+double temperatura1, temperatura2 = 0;
+int sample = 500;
+
 void setup()
 {
   Serial.begin(115200);
@@ -21,12 +28,21 @@ void loop()
 {
   if (Serial.available())
   {
+    //Read the data and plus to variable average
+    for (int index = 0; index < sample; index++)
+    {
+      double temperature1 = temp1.getTemp(resistor);
+      temperatura1 += temperature1;
+      double temperature2 = temp2.getTemp(resistor);
+      temperatura2 += temperature2;
+      // a small delay for read again
+      delayMicroseconds(1);
+    }
     myStats[0].add(readphoto(0));
     myStats[1].add(readphoto(1));
     myStats[2].add(readphoto(2));
     myStats[3].add(readphoto(3));
-    myStats[4].add(get_temp1());
-    myStats[5].add(get_temp2());
+
     if (simulated == true)
     {
       Serial.flush();
@@ -38,28 +54,32 @@ void loop()
         // check if return keyboard ware pressed
         if (character == '\n')
         {
-            for (int i = 0; i < 6; i++)
-            {
-              /*
+          for (int i = 0; i < 4; i++)
+          {
+            /*
                 Serial.print(myStats[i].minimum(), 4);
                 Serial.print(" ");
                 Serial.print(myStats[i].maximum(), 4);
                 Serial.print(" ");
                 */
-              Serial.print(myStats[i].average(), 4);
-              Serial.print(" ");
-            }
-            myStats[0].clear();
-            myStats[1].clear();
-            myStats[2].clear();
-            myStats[3].clear();
-            myStats[4].clear();
-            myStats[5].clear();
-            Serial.println();
+
+            Serial.print(myStats[i].average(), 4);
+            Serial.print(" ");
           }
-          information.concat(character);
-          delay(10);
-        
+          temperatura1 = temperatura1 / sample;
+          temperatura2 = temperatura2 / sample;
+          Serial.print(temperatura1, 2);
+          Serial.print(" ");
+          Serial.print(temperatura2, 2);
+          Serial.print(" ");
+          myStats[0].clear();
+          myStats[1].clear();
+          myStats[2].clear();
+          myStats[3].clear();
+          Serial.println();
+        }
+        information.concat(character);
+        delay(10);
       }
     }
     else
@@ -71,7 +91,7 @@ void loop()
         /*
         varredura dos indices dos sensores
         */
-        for (int i = 0; i <= 5; i++)
+        for (int i = 0; i < 4; i++)
         {
           /*
           Serial.print(myStats[i].minimum(), 4);
@@ -82,14 +102,16 @@ void loop()
           Serial.print(myStats[i].average(), 4);
           Serial.print(" ");
         }
-
+        temperatura1 = temperatura1 / sample;
+        temperatura2 = temperatura2 / sample;
+        Serial.print(temperatura1, 2);
+        Serial.print(" ");
+        Serial.print(temperatura2, 2);
+        Serial.print(" ");
         myStats[0].clear();
         myStats[1].clear();
         myStats[2].clear();
         myStats[3].clear();
-        myStats[4].clear();
-        myStats[5].clear();
-
         Serial.println();
       }
     }
