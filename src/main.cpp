2aa0a99e05f229cecaeb04ce08a686180db898b7
marciogarridoLaCop <marciogarrido@id.uff.br>
2022-02-05 12:05:16 -0300

    SNR

--- src/main.cpp
+++ src/main.cpp
@@ -7,59 +7,51 @@ Thermistor temp2(6);
 // value of resistor in ohms
 double resistor = 10000;
 
-int readvalue, readvalue1, readvalue2, readvalue3, readvalue4 = 0; // Ler valor porta
+int readvalue = 0; // Ler valor porta
 float volts = 0;
 
-// variable to store the average of read
-double average1 = 0;
-double average2 = 0;
 
 // number of samples
+<<<<<<< HEAD
 int sample = 1000;
+=======
+int sample = 1;
+>>>>>>> 8b74db9f76ae5f8b4e4b665459b4416bb40a54cd
 
 void setup()
 {
-  Serial.begin(115200);
+  Serial.begin(2000000);
 }
 
 void loop()
 {
-  // Read the data and plus to variable average
-  for (int index = 0; index < sample; index++)
-  {
-    double temperature1 = temp1.getTemp(resistor);
-    double temperature2 = temp2.getTemp(resistor);
-    average1 += temperature1;
-    average2 += temperature2;
-    delayMicroseconds(1);
-  }
-
-  readvalue = analogRead(0);
-  volts = readvalue * (5.0 / 1023.0);
-  Serial.print(volts, 2);
+  readvalue = analogRead(0); 
+  Serial.print(readvalue * (5.0 / 1023.0);, 2);
   Serial.print(" ");
 
   readvalue = analogRead(1);
-  volts = readvalue * (5.0 / 1023.0);
-  Serial.print(volts, 2);
+  Serial.print(readvalue * (5.0 / 1023.0);, 2);
   Serial.print(" ");
 
   readvalue = analogRead(2);
-  volts = readvalue * (5.0 / 1023.0);
-  Serial.print(volts, 2);
+  Serial.print(readvalue * (5.0 / 1023.0);, 2);
   Serial.print(" ");
 
   readvalue = analogRead(3);
-  volts = readvalue * (5.0 / 1023.0);
-  Serial.print(volts, 2);
+  Serial.print(readvalue * (5.0 / 1023.0);, 2);
   Serial.print(" ");
+<<<<<<< HEAD
 
   average1 = average1 / sample;
   average2 = average2 / sample;
   Serial.print(average1, 1);
   Serial.print(" ");
   Serial.print(average2, 1);
+=======
+ 
+  Serial.print(temp1.getTemp(resistor);, 0);
+  Serial.print(" ");
+  Serial.print(temp2.getTemp(resistor);, 0);
+>>>>>>> 8b74db9f76ae5f8b4e4b665459b4416bb40a54cd
   Serial.println();
-
-  delay(10);
 }
